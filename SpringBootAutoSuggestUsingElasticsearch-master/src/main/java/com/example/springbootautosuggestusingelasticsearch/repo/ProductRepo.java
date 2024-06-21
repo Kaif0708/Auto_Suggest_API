9c8b24b0155d5711021c93cd3bd3a478312a1570
Kaif0708 <110331551+Kaif0708@users.noreply.github.com>
2024-06-21 18:28:56 +0530

    Delete SpringBootAutoSuggestUsingElasticsearch-master directory

--- SpringBootAutoSuggestUsingElasticsearch-master/src/main/java/com/example/springbootautosuggestusingelasticsearch/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.springbootautosuggestusingelasticsearch.repo;
-
-import com.example.springbootautosuggestusingelasticsearch.entity.Product;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductRepo extends ElasticsearchRepository<Product,Integer> {
-}
